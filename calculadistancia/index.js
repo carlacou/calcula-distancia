3eed0e203c9c6eaff11cbd5dd4233b8971abc573
carlacou <65657145+carlacou@users.noreply.github.com>
2021-03-08 02:31:51 -0300

    Add files via upload

---
+++ calculadistancia/index.js
@@ -0,0 +1,12 @@
+function formatamoeda(numero){
+
+  return numero.toFixed(2).replace(".",",")
+}
+
+function calcular(){
+
+  distanciakm= inputDistancia.value
+  tempomin= inputTempo.value
+  valorcorrida=2+distanciakm*1.4+tempomin*0.26
+  botaoCalcular.innerHTML='Total: R$'+formatamoeda(valorcorrida)
+}
